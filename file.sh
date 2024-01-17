4aa4f2e3a076229d090d2c98e336ac08a19f5ef6
Altamsh Bairagdar <84860267+altamsh04@users.noreply.github.com>
2024-01-18 01:01:03 +0530

    Update file.sh, make code efficient and readable

--- file.sh
+++ file.sh
@@ -1,152 +1,149 @@
 #!/bin/bash
-echo ""
-echo ""
-echo "********|---------------------------|********"
-echo "********| File Manupulation In Bash |********"
-echo "********|---------------------------|********"
-echo ""
-echo ""
-echo "1.Search file or directory."
-echo "2.Create or delete file or directory."
-echo "3.Get or Set permission."
-echo ""
-echo ""
-read -p "Enter your choice (1/2/3) : " ch
 
-case $ch in
-###1###
-1)
-echo
-echo "***Search file or directory***"
-echo
-read -p "Enter File Name : " search
+print_separator() {
+    echo "********|---------------------------|********"
+}
 
-if [ -f $search ];
-then 
-	echo "File exist !"
+print_menu_header() {
+    print_separator
+    echo "********| File Manipulation In Bash |********"
+    print_separator
+}
 
-elif [ -d $search ];
-then
-	echo "Directory exist"
-else
-	echo "Not exist !"
-fi
-;;
+print_menu_options() {
+    echo ""
+    echo "1. Search file or directory."
+    echo "2. Create or delete file or directory."
+    echo "3. Get or set permission."
+    echo ""
+}
 
-###2###
-2)
-echo
-echo "***Create or delete file***"
-echo
+search_file_or_directory() {
+    echo "***Search file or directory***"
+    echo
+    read -p "Enter File Name : " search
 
-echo "1.Create file."
-echo "2.Delete file."
-read -p "Enter your choice : " ch2
+    if [ -f "$search" ]; then 
+        echo "File exists!"
+    elif [ -d "$search" ]; then
+        echo "Directory exists."
+    else
+        echo "Not found!"
+    fi
+}
 
-	case $ch2 in 
-	1)
-	echo
-	echo "***Create file***"
-	echo
-	read -p "Enter file name : " create
-	if [ -f $create ];
-	then
-		echo "File already exist !"
-	else
-		$(touch $create)
-		echo
-		echo $(ls)
-		echo
-		echo "File created !"
-	fi
-	;;
-	2)
-	echo "***Delete file***"
-	echo
-	read -p "Enter file name : " delete
-	if [ -f $delete ];
-	then	
-		$(rm $delete)
-		echo	
-		echo $(ls)
-		echo
-		echo "File deleted !"
-	else
-		echo "File not found !"
-	fi
-	;; 
-	esac
-;;
+create_or_delete_file() {
+    echo "***Create or delete file***"
+    echo
+    echo "1. Create file."
+    echo "2. Delete file."
+    read -p "Enter your choice : " ch2
 
-###3###
-3)
-echo
-echo "***Get or set permission***"
-echo
+    case $ch2 in 
+        1)
+            create_file
+            ;;
+        2)
+            delete_file
+            ;;
+        *)
+            echo "Invalid choice!"
+            ;;
+    esac
+}
 
-echo "1.Get Permission."
-echo "2.Set Permission."
-read -p "Enter your choice : " ch3
+create_file() {
+    echo "***Create file***"
+    echo
+    read -p "Enter file name : " create
 
-	case $ch3 in 
-	1)
-	echo
-	echo "***Get Permission***"
-	echo	
-	read -p "Enter file name or directory : " per
-	if [ -f $per ];
-	then
-		echo "$per permission is !"
-		echo
-		echo $(ls $per -l)
-	elif [ -d $per ];
-	then
-		echo "$per permission is !"
-		echo
-		echo $(ls $per -l)
-	else
-		echo "Not found !"
-	fi
-	;;
-	2)
-	echo
-	echo "***Set Permission***"
-	echo		
-	echo "1.Read"
-	echo "2.Write"
-	echo "3.Execute"
-	echo "4.All"
-	echo	
-	read -p "Enter your choice : " ch4
-	echo
-	read -p "Enter file name or directory : " mod
-	echo
-	case $ch4 in 
-	1)
-	echo
-	$(chmod 4 $mod)
-	echo "Read permission set !"
-	;;
-	2)
-	echo
-	$(chmod 5 $mod)
-	echo "Write permission set !"
-	;;
-	3)
-	echo
-	$(chmod 7 $mod)	
-	echo "Execute permission set !"
-	;;	
-	4)
-	echo
-	$(chmod 777 $mod)	
-	echo "All permission set !"
-	;; 
-	esac
-;;
-*)
-	echo "Invalid choice !"
-;;
-esac
-;;
-esac
+    if [ -f "$create" ]; then
+        echo "File already exists!"
+    else
+        touch "$create"
+        ls
+        echo "File created!"
+    fi
+}
+
+delete_file() {
+    echo "***Delete file***"
+    echo
+    read -p "Enter file name : " delete
+
+    if [ -f "$delete" ]; then
+        rm "$delete"
+        ls
+        echo "File deleted!"
+    else
+        echo "File not found!"
+    fi
+}
+
+get_or_set_permission() {
+    echo "***Get or set permission***"
+    echo
+    echo "1. Get Permission."
+    echo "2. Set Permission."
+    read -p "Enter your choice : " ch3
+
+    case $ch3 in 
+        1)
+            get_permission
+            ;;
+        2)
+            set_permission
+            ;;
+        *)
+            echo "Invalid choice!"
+            ;;
+    esac
+}
+
+get_permission() {
+    echo "***Get Permission***"
+    echo
+    read -p "Enter file name or directory : " per
+
+    if [ -e "$per" ]; then
+        echo "$per permission is:"
+        ls -l "$per"
+    else
+        echo "Not found!"
+    fi
+}
+
+set_permission() {
+    echo "***Set Permission***"
+    echo
+    echo "1. Read"
+    echo "2. Write"
+    echo "3. Execute"
+    echo "4. All"
+    read -p "Enter your choice : " ch4
+    echo
+    read -p "Enter file name or directory : " mod
+
+    case $ch4 in 
+        1) chmod 4 "$mod"; echo "Read permission set!";;
+        2) chmod 5 "$mod"; echo "Write permission set!";;
+        3) chmod 7 "$mod"; echo "Execute permission set!";;
+        4) chmod 777 "$mod"; echo "All permission set!";;
+        *) echo "Invalid choice!";;
+    esac
+}
+
+main() {
+    print_menu_header
+    print_menu_options
+    read -p "Enter your choice (1/2/3): " ch
+
+    case $ch in
+        1) search_file_or_directory ;;
+        2) create_or_delete_file ;;
+        3) get_or_set_permission ;;
+        *) echo "Invalid choice!" ;;
+    esac
+}
+
+main
